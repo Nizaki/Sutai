6ce1dfc82f1f6d6d9b087efacb8878eb1187c56c
DNA.LS <NizakiFX@gmail.com>
2021-06-06 02:15:27 +0700

    add missing file

---
+++ Assets/Scripts/obj/PowerUpObj.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PowUp", menuName = "Game/PowerUp")]
+public class PowerUpObj : ScriptableObject
+{
+    public Sprite image;
+    [Multiline]
+    public string description;
+    public List<PowerAttribute> power;
+}
+//test
+[Serializable]
+public class PowerAttribute
+{
+    public PowerType type;
+    public float value;
+}
+
+public enum PowerType
+{
+    Health,
+    Speed,
+    Heal,
+    ShootSpeed,
+    ShootFlySpeed,
+    ShootDamage,
+    ShootLevel,
+    Barrier
+}
